ff6c252cdb4e3330567cfeb0083046ce5cb88f15
SyedMustafaImam <cs1812134@szabist.pk>
2021-03-07 16:16:01 +0500

    server changes

--- Practice/Complete NodeJS Course With exp, socket.io and mgDB/4. Build a Todo App/app.js
+++ Practice/Complete NodeJS Course With exp, socket.io and mgDB/4. Build a Todo App/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 
-app.listen(3000);
+app.listen(9854);
 
 app.get('/', (req, res) => {
     res.sendFile('./pages/index.html', { root: __dirname });
