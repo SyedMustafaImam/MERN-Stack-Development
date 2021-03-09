9e375052a95bf19b8d99eb04efbe72fa2f6a6b66
SyedMustafaImam <cs1812134@szabist.pk>
2021-03-10 01:54:08 +0500

    deploying to server

--- class_work/firstClass/app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type':'text/plain'})
-res.end("mustafa");
-console.log("we are using prot 8000");
-}).listen(8000, ()=>{
-    console.log("This server is working fine")
-})
